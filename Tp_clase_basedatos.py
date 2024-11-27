a7123f29086f98eb89dbb389b02668b9c342a7fa
Jose Luis Pequez <pequezcarlos@gmail.com>
2024-11-27 01:44:04 -0300

    Update Tp_clase_basedatos.py

--- Tp_clase_basedatos.py
+++ Tp_clase_basedatos.py
@@ -15,21 +15,14 @@ class BasededatosDocumental (object):
     def obtener_coleccion(self ,nombre_coleccion):
         return self.colecciones.get(nombre_coleccion,None)
     
-    def importar_csv ():
-        direccion = "D:\CURSADA\orientacion a objetos\projecto_1.1\datos_personales.csv"
-        with open(direccion, 'rt') as file:  
-            schema = file.readline().replace("\n", "") 
-            parser = string_A_dicc(schema) 
-            i= 0 
-            line= file.readline() 
- 
-            while line != "": 
-                d=Documento(i,parser.convertidor(line)) 
-                self.agregar_documento(d) 
-                i = i+1 
-                line= file.readline()
+
     def __str__(self):
         return f"Coleccion{self.nombre} con {len(self.documentos)} documentos."
     
 
+D = Coleccion ("libros")
+
+a = BasededatosDocumental()
+
+a.crear_coleccion("pelis")
 
