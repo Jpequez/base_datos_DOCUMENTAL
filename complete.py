dde6784d1f2d70d8cafffebd7b8badb0df50ee86
Jose Luis Pequez <pequezcarlos@gmail.com>
2024-11-27 01:44:02 -0300

    Update complete.py

--- complete.py
+++ complete.py
@@ -81,3 +81,54 @@ else:
 
 '''// se creo la clase collecion que manejara los documentos
 puede agregar,eliminar,buscar'''
+
+
+'''3.creamos la clase base de datos  '''
+
+class BasededatosDocumental (object):
+    def __init__(self):
+        self.colecciones= {}
+
+    def crear_coleccion (self,nombre_coleccion):
+        if nombre_coleccion not in self.colecciones:
+            self.colecciones[nombre_coleccion] = Coleccion(nombre_coleccion)
+
+    def eliminar_coleccion (self, nombre_coleccion):
+        if nombre_coleccion in self.colecciones:
+            del self.colecciones[nombre_coleccion]
+
+    def obtener_coleccion (self ,nombre_coleccion):
+        return self.colecciones.get(nombre_coleccion,None)
+    
+    def __str__(self):
+        return f"Coleccion{self.nombre} con {len(self.documentos)} documentos."
+    
+
+''' manejara las colecciones ,, tendra capacidad de eliminar y mostrar las colecciones existentes de al base de datos '''
+
+
+
+''' clase string a diccionario '''
+schema = "Nombre,Apellido,Edad,Mail,altura"
+row = "jose,pequez,22,pequezjoseluis@gmail.com,2.23"
+
+class string_A_dicc(object):
+    def __init__(self,schemastr,separator= ","):
+        self.schema = schemastr.split(separator)
+        self.seperator = separator
+
+
+    def convertidor (self, row):
+        tempo = row.split(self.seperator)
+        if len(tempo)== len(self.schema):
+         i = 0 
+         dicc = {}
+         while i < len(tempo):
+            #dicc[self.schema[i]] = tempo[i]
+            key = self.schema[i]
+            val = tempo[i]
+            dicc[key] = val
+            i = i+1
+        return dicc
+    
+''' permite la conversion de un archivo csv a diccionario '''
