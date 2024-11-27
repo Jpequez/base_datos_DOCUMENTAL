4a4031baac0f2c8df7e45650255879693e356c7a
Jose Luis Pequez <pequezcarlos@gmail.com>
2024-11-27 01:23:36 -0300

    Update Tp_clase_basedatos.py

--- Tp_clase_basedatos.py
+++ Tp_clase_basedatos.py
@@ -1,7 +1,7 @@
 from Tp_clase_coleccion import  Coleccion
 
 class BasededatosDocumental (object):
-    def __init__(self):
+    def __init__ (self):
         self.colecciones= {}
 
     def crear_coleccion (self,nombre_coleccion):
@@ -18,3 +18,9 @@ class BasededatosDocumental (object):
     def __str__(self):
         return f"Coleccion{self.nombre} con {len(self.documentos)} documentos."
     
+
+D = Coleccion ("libros")
+
+a = BasededatosDocumental()
+
+a.crear_coleccion("pelis")
